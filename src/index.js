e09d28db4b04fd432026d63e7bd3fdd60e506d47
Aman Kumar <akgupta0524@gmail.com>
2024-08-20 21:13:56 +0530

    added app.js app.css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "slick-carousel/slick/slick.css"; // Slick Carousel CSS
+import "slick-carousel/slick/slick-theme.css"; // Slick Carousel theme CSS
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
